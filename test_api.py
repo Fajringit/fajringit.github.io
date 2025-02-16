b36aed62cb787ef26585fadd4e1756b6ef3b1cb2
Fajrin Riyanto <fajrin29riyanto@gmail.com>
2025-02-16 20:22:36 +0700

    update test_api.py to look status code

--- test_api.py
+++ test_api.py
@@ -4,10 +4,17 @@ BASE_URL = "http://127.0.0.1:5000/users"
 
 # 1. POST - Create User
 def test_create_user():
-    data = {"name": "Fajrin", "email": "fajrin@example.com"}
+    data = {
+        "name": "Fajrin",
+        "email": "fajrin@example.com"
+    }
     response = requests.post(BASE_URL, json=data)
+
+    print("Status Code:", response.status_code)
+    print("Response Body:", response.text)  # Lihat detail error API
+
     assert response.status_code == 201
-    print("Create User:", response.json())
+
 
 # 2. GET - Get All Users
 def test_get_users():
@@ -37,6 +44,6 @@ def test_delete_user(user_id):
 if __name__ == "__main__":
     test_create_user()  # Test Create User
     test_get_users()    # Test Get All Users
-    test_get_user_by_id(4)  # Test Get User by ID (assumes ID 1 exists)
-    test_update_user(4)  # Test Update User (assumes ID 1 exists)
-    test_delete_user(4)  # Test Delete User (assumes ID 1 exists)
+    test_get_user_by_id(2)  # Test Get User by ID (assumes ID 1 exists)
+    test_update_user(2)  # Test Update User (assumes ID 1 exists)
+    test_delete_user(2)  # Test Delete User (assumes ID 1 exists)
